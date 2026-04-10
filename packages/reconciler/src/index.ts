71a89dd4ccbc4f5b81a2e64db56b1bfa173bc654
Brandon Pierce <brandonpierce91@gmail.com>
2026-04-10 13:42:57 -0400

    fix: shift side effects import

--- packages/reconciler/src/index.ts
+++ packages/reconciler/src/index.ts
@@ -1,5 +1,4 @@
+import './side-effects';
 export { createRoot, unmountAtNode, roots } from './renderer';
 export { extend } from './extend';
 export type { ReconcilerRoot } from './types';
-
-import './side-effects';
